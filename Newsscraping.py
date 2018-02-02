9203db177ce4afb2cd9911d14e4818f4007783ad
Ninad <ninad@Ninads-MacBook-Pro.local>
2018-02-02 18:12:07 +0530

    fixed the string and added MAC notification

--- Newsscraping.py
+++ Newsscraping.py
@@ -1,6 +1,14 @@
 import urllib.request
 import requests
+import os
 from bs4 import BeautifulSoup
+
+
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
 
 
 url = "https://www.inshorts.com/en/read"
@@ -17,7 +25,7 @@ for each_div in containers:
     span = each_div.find_all('span')
     headline_text = str(span[0].text)
     headline.append(headline_text)
-    #print(type(headline_text))
+    
     
 
 with open('headlines.txt', 'w') as w:
@@ -29,4 +37,19 @@ with open('headlines.txt','r') as f:
     main_headline = f.read()
 
 formatted = list(main_headline.strip('[').split(','))
-print(formatted[5])
+
+final_out = []
+for each_line in range(len(formatted)):
+    new_string = formatted[each_line].replace("'", "")
+    final_out.append(new_string)
+
+notify("News Update", final_out[0])
+
+
+
+
+
+
+
+
+
